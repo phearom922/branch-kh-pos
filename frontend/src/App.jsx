14484f9198d072a03b96c5cf0d340bf3379af7b2
phearom <ronphearom2540@gmail.com>
2025-09-05 18:00:08 +0700

    update loading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,10 +17,11 @@ const AppContent = () => {
   const { user, loading } = useContext(AuthContext);
 
   if (loading) {
-    console.log("AppContent: Loading user data..."); // Debug
+    console.log("AppContent: Loading user data..."); 
     return (
-      <div className="min-h-screen flex items-center justify-center bg-gray-100">
-        <div className="w-8 h-8 border-4 border-primary border-t-transparent rounded-full animate-spin"></div>
+      <div className="min-h-screen flex items-center justify-center bg-gray-100 flex-col gap-5">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary"></div>
+        <p className="text-gray-500 font-semibold ">AppContent: Loading user data...</p>
       </div>
     );
   }
